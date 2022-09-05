dc99181ecbe636a8a31660b5bf73302c22acc6e1
Jheovany Menjivar <ejmenjiv@bancolombia.com.co>
2022-09-04 19:39:59 -0600

    first commit

---
+++ lib/src/main/java/com/enterprise/kafkamodels/HobbitQuotes.java
@@ -0,0 +1,11 @@
+package com.enterprise.kafkamodels;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class HobbitQuotes {
+    private int id;
+    private String quote;
+}
